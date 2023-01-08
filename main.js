f6dc0bc36a49dfdf125b64d5cbd4b7cc6fff4073
iamericfletcher <EricFletcher3@gmail.com>
2023-01-08 17:20:36 -0500

    Added SMS + Email notifications for when the cache is null or stall so I know when server issues exist.

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 import express from "express"
 import dotenv from "dotenv"
-import { updateCache } from "./src/scrapper.js"
-import { cache } from "./src/cache.js"
-import { prisma } from "./src/client.js"
+import {updateCache} from "./src/scrapper.js"
+import {cache} from "./src/cache.js"
+import {prisma} from "./src/client.js"
 import fetch from "node-fetch"
 import bodyParser from "body-parser"
-
+import TwilioClient from "twilio";
+import mailgun from "mailgun-js";
 
 dotenv.config()
+
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const client = TwilioClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN;
+const MAILGUN_API_KEY = process.env.MAILGUN_API_KEY;
+const mg = mailgun({apiKey: MAILGUN_API_KEY, domain: MAILGUN_DOMAIN});
+
+
 
 updateCache()
 setInterval(updateCache, 6000000)
@@ -19,18 +30,41 @@ const port = 3001
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-	if (cache.value.lastScraped === null) {
-		return res.status(500).json({
-			message: 'Cache not available'
-		})
-	}
-	res.json(cache.value)
+    // Notify Eric via email and SMS that something is wrong with the server
+    if (cache.value.lastScraped === null || cache.value.lastScraped.toString().slice(0, 10) !== new Date().toISOString().slice(0, 10)) {
+        console.log("Cache Null or Stale - Sending notifications to Eric Fletcher");
+        const data2 = {
+            // from: 'Excited User <me@samples.mailgun.org>',
+            from: `bpl-pass-notification@${MAILGUN_DOMAIN}`,
+            to: process.env.ERIC_EMAIL,
+            // to: 'bar@example.com, YOU@YOUR_DOMAIN_NAME',
+            subject: 'Boston Public Library Museum Issue Notification',
+            text: 'Cache is null or stale - please check the server' +
+                new URL("https://bpl-museum-passes.vercel.app/")
+        };
+        mg.messages().send(data2, function (error, body) {
+            console.log("Email Body: " + body);
+        });
+        client.messages
+            .create({
+                body: 'Cache is null or stale - please check the server' +
+                    new URL("https://bpl-museum-passes.vercel.app/"),
+                from: '+18145606408',
+                to: process.env.ERIC_PHONE,
+            })
+            .then(message => console.log("Phone Message SID: " + message.sid));
+        return res.status(500).json({
+            message: 'Cache not available'
+        })
+    }
+    res.json(cache.value)
 })
 
 app.post('/', async (req, res) => {
     // Google Recaptcha check
     const human = validateHuman(req.body.token);
     const recaptchaSuccess = await human;
+
     // console.log("TOKEN IS: ", req.body.token);
 
     async function validateHuman(token) {
@@ -69,5 +103,5 @@ app.post('/', async (req, res) => {
 })
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
